009ce0fb0c64b716ab1fc2dfb54faf5a048b6c58
kamrankh068 <kamrankh068@gmail.com>
2020-05-31 21:27:23 +0530

    java source code

---
+++ Add.java
@@ -0,0 +1,10 @@
+// Presented by IT-EXPERTS FOUNDATION
+//itexpert127001@gmail.com
+
+
+public class Add {
+public static void main(String[] args){
+int x=1, y=2 ,z;
+z=x+y;
+System.out.println("the sum of x and y is " + z);
+}}
---
+++ Dog.java
@@ -0,0 +1,7 @@
+// Presented by IT-EXPERT FOUNDATION
+//itexpert127001@gmail.com
+
+public class Dog {
+ public static void main(String[] args){
+System.out.println("hello");
+}}
